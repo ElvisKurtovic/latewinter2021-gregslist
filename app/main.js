413851c4d42beebd807fbcae8e4f85b65ab08ff6
ElvisKurtovic <elvis.kurtovic23@gmail.com>
2021-02-17 16:15:33 -0700

    finished site with nav bars and new font

--- app/main.js
+++ app/main.js
@@ -5,8 +5,23 @@ import HousesController from "./Controllers/HousesController.js";
 
 
 
+function startTabs() {
+  var triggerTabList = [].slice.call(document.querySelectorAll('#navCartab, #navHousetab, #navJobstab'))
+
+  triggerTabList.forEach(function (triggerEl) {
+    var tabTrigger = new bootstrap.Tab(triggerEl)
+    triggerEl.addEventListener('click', function (event) {
+
+      event.preventDefault()
+      tabTrigger.show()
+    })
+  })
+}
 class App {
   // valuesController = new ValuesController();
+  constructor() {
+    startTabs()
+  }
   carsController = new CarsController();
   jobsController = new JobsController();
   housesController = new HousesController();
